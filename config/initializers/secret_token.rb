5c2ffe3b2af29e5785e1a63686dfcae43dd80381
Javier Noris <norisjavier@gmail.com>
2014-03-24 02:50:39 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditOnRails::Application.config.secret_key_base = '4421fd04fa1c1b4cbd4c2dd62734877054268942246b2feb59ae9e5db10b5f74082304630fca97c0c73a93cbcf95646953147493649ac9beacbae6cb70242002'
