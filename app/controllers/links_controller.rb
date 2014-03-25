805b08ec5e40f606a0d3f8cfce933da05d85b8e6
Javier Noris <norisjavier@gmail.com>
2014-03-24 18:59:12 -0700

    Added Show Pages For Links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
 
   def index
+  	@link = Link.all
   end
 
   def new
@@ -8,6 +9,37 @@ class LinksController < ApplicationController
   end
 
   def create
+    @link = Link.new(link_params)
+
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @link }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end  
   end
 
+  def show	
+  	@link = Link.find(params[:id])
+  end
+
+
+private
+  def set_link
+    @link = Link.find(params[:id])
+  end
+  def link_params
+  	params.require(:link).permit(:title, :url)
+  end
+
+
 end
+
+
+# Addint a new link on our new view/action isn't 
+# updating our database. So after adding a new link 
+# we are redirected to our index that shows our database
+# of links table?
